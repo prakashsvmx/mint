ceb70c5ae1fb1fd474aa5b964ab247ce4470768d
Prakash <prakash@minio.io>
2021-11-22 13:14:50 +0530

    JS test

--- build/minio-js/install.sh
+++ build/minio-js/install.sh
@@ -15,7 +15,7 @@
 #  limitations under the License.
 #
 
-MINIO_JS_VERSION=$(curl --retry 10 -Ls -o /dev/null -w "%{url_effective}" https://github.com/minio/minio-js/releases/latest | sed "s/https:\/\/github.com\/minio\/minio-js\/releases\/tag\///")
+MINIO_JS_VERSION=$(curl --retry 10 -Ls -o /dev/null -w "%{url_effective}" https://github.com/prakashsvmx/minio-js/7.0.17 | sed "s/https:\/\/github.com\/prakashsvmx\/minio-js\/releases\/tag\///")
 if [ -z "$MINIO_JS_VERSION" ]; then
     echo "unable to get minio-js version from github"
     exit 1
@@ -23,6 +23,6 @@ fi
 
 test_run_dir="$MINT_RUN_CORE_DIR/minio-js"
 mkdir "${test_run_dir}/test"
-$WGET --output-document="${test_run_dir}/test/functional-tests.js" "https://raw.githubusercontent.com/minio/minio-js/${MINIO_JS_VERSION}/src/test/functional/functional-tests.js"
+$WGET --output-document="${test_run_dir}/test/functional-tests.js" "https://raw.githubusercontent.com/prakashsvmx/minio-js/7.0.17/src/test/functional/functional-tests.js"
 npm --prefix "$test_run_dir" install --save "minio@$MINIO_JS_VERSION"
 npm --prefix "$test_run_dir" install
